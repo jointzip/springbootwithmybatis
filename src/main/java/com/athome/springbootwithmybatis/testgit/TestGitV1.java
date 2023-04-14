7e123a8c2ed99adf5fed6e2a72ffd789448e2102
zhaopo <1336134195@qq.com>
2023-04-14 14:08:10 +0800

    测试新建文件的提交

--- src/main/java/com/athome/springbootwithmybatis/testgit/TestGitV1.java
+++ src/main/java/com/athome/springbootwithmybatis/testgit/TestGitV1.java
@@ -7,5 +7,6 @@ public class TestGitV1 {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("回退之前的最后一次版本");
+        System.out.println("测试暂存区stage");
     }
 }
