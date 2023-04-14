dead50225575030b44ce89a28fb5c857ed5b01ec
zhaopo <1336134195@qq.com>
2023-04-14 13:39:24 +0800

    回退之前的最后一次版本

--- src/main/java/com/athome/springbootwithmybatis/testgit/TestGitV1.java
+++ src/main/java/com/athome/springbootwithmybatis/testgit/TestGitV1.java
@@ -6,5 +6,6 @@ package com.athome.springbootwithmybatis.testgit;
 public class TestGitV1 {
     public static void main(String[] args) {
         System.out.println("第一次提交");
+        System.out.println("回退之前的最后一次版本");
     }
 }
